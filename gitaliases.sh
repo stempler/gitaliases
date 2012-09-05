42daffd656d9d3d983283b7b488df72a2cd8c926
Simon Templer <simon@templer.cc>
2012-09-05 10:25:47 +0200

    commented out aliases I don't like

--- gitaliases.sh
+++ gitaliases.sh
@@ -1,16 +1,16 @@
 # aliases for common git commands
 alias g='git'
-alias st='git status'
+#alias st='git status'
 alias push='git push'
-alias commit='git commit'
-alias log='git log'
-alias stash='git stash'
-alias checkout='git checkout'
-alias branch='git branch'
-alias fetch='git fetch'
-alias merge='git merge'
-alias cherry-pick='git cherry-pick'
-alias rebase='git rebase'
+#alias commit='git commit'
+#alias log='git log'
+#alias stash='git stash'
+#alias checkout='git checkout'
+#alias branch='git branch'
+#alias fetch='git fetch'
+#alias merge='git merge'
+#alias cherry-pick='git cherry-pick'
+#alias rebase='git rebase'
 
 # 'git pull --rebase' with a short log of the latest changes
 pull () { local HEADHASH=`git describe --always --abbrev=40`; git pull --rebase $*; git submodule update; echo; PAGER='cat -B' git log --format="%C(yellow)%h %C(green)%an%C(reset): %s" $HEADHASH.. | sed -nr 's/([^:]+)\:/\1\t/;p'; }
@@ -22,7 +22,7 @@ ff () { local HEADHASH=`git describe --always --abbrev=40`; git pull --ff-only $
 add () { git add -v $*; git status; }
 
 # verbose reset
-reset () { git reset $*; git status; }
+#reset () { git reset $*; git status; }
 
 # 'git add' with 'git ls-files' and grep
 # usage like grep - example: gadd 'readme.txt'
